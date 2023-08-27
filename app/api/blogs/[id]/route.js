86e039a1750291adf62c5b611e3a3a11ea16c026
nitish0104 <nitishdalvi1@gmail.com>
2023-08-27 09:27:49 +0530

    Backend Completed

--- app/api/blogs/[id]/route.js
+++ app/api/blogs/[id]/route.js
@@ -9,3 +9,11 @@ export async function PUT(req, { params }) {
 	await Blog.findByIdAndUpdate(id, { title, description })
 	return NextResponse.json({ message: "Updated" }, { status: 200 })
 }
+
+export async function GET({ params }) {
+	const { id } = params
+	await connectMongoDb()
+	const blog = await Blog.findOne({ _id: id })
+	return NextResponse.json({ blog }, { status: 200 })
+
+}
